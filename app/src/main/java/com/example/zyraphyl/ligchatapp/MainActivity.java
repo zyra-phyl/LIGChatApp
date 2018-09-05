730affe4dabd6087e7df9d6c0c1ed6f00ca55a44
Zyraphyl Penaranda <zlpearanda@up.edu.ph>
2018-09-05 15:55:49 +0800

    Create Basic User Interface

--- app/src/main/java/com/example/zyraphyl/ligchatapp/MainActivity.java
+++ app/src/main/java/com/example/zyraphyl/ligchatapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.zyraphyl.ligchatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.signin.SignIn;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignIn.class));
+            finish();
+            return;
+        } else {
+
+        }
     }
 }
