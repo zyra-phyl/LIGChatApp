f3e357afe1198383545b1fac152134ebc5f89dca
Zyraphyl Penaranda <zlpearanda@up.edu.ph>
2018-09-06 13:39:14 +0800

    Finish Dashboard

--- app/src/main/java/com/example/zyraphyl/ligchatapp/MainActivity.java
+++ app/src/main/java/com/example/zyraphyl/ligchatapp/MainActivity.java
@@ -1,33 +1,44 @@
 package com.example.zyraphyl.ligchatapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v4.graphics.TypefaceCompatUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.signin.SignIn;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private Button signup,login;
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize Firebase Auth
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        if (mFirebaseUser == null) {
-            // Not signed in, launch the Sign In activity
-            startActivity(new Intent(this, SignIn.class));
-            finish();
-            return;
-        } else {
+        Intent loadDash = new Intent(this,Dashboard.class);
+        startActivity(loadDash);
 
-        }
+        //Initialize Layouts
+        LayoutElements();
+    }
+    private void LayoutElements(){
+        Typeface arial = Typeface.createFromAsset(getAssets(), "fonts/arial.ttf");
+        login = (Button) findViewById(R.id.loginButtonMain);
+        login.setTypeface(arial);
+        signup = (Button) findViewById(R.id.signUpButtonMain);
+        signup.setTypeface(arial);
+        appName = (TextView) findViewById(R.id.appName);
+        appName.setTypeface(arial);
     }
 }
